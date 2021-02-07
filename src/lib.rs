2447559bae6471c4b7fd22102584719cf0f1b60e
philip.a.markgraf <philip_markgraf@mac.com>
2021-02-07 11:41:20 -0800

    f put, then is_empty

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,17 @@
-struct CircularBuffer {}
+struct CircularBuffer {
+    num_elem: usize,
+}
 
 impl CircularBuffer {
     pub fn new(_: usize) -> CircularBuffer {
-        CircularBuffer {}
+        CircularBuffer { num_elem: 0 }
     }
-    pub fn is_empty(&self) -> bool { true }
+    pub fn is_empty(&self) -> bool { self.num_elem == 0 }
     pub fn is_full(&self) -> bool { false }
-    pub fn put(&self, _: i32) -> bool { true }
+    pub fn put(&mut self, _: i32) -> bool {
+        self.num_elem = self.num_elem + 1;
+        true
+    }
 }
 
 #[cfg(test)]
@@ -20,8 +25,9 @@ mod tests {
         assert_eq!(false, b.is_full());
     }
     #[test]
-    fn given_capacity_one_when_put_then_return_true() {
-        let b = CircularBuffer::new(1);
+    fn given_capacity_one_when_put_then_return_true_is_empty_false() {
+        let mut b = CircularBuffer::new(1);
         assert_eq!(true, b.put(42));
+        assert_eq!(false, b.is_empty());
     }
 }
