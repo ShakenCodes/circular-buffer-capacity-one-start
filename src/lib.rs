edfd7992ef3b252e5b0c9bc6d089ffd40ae7c1cb
philip.a.markgraf <philip_markgraf@mac.com>
2021-02-07 11:37:28 -0800

    f put

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ impl CircularBuffer {
     }
     pub fn is_empty(&self) -> bool { true }
     pub fn is_full(&self) -> bool { false }
+    pub fn put(&self, _: i32) -> bool { true }
 }
 
 #[cfg(test)]
@@ -17,5 +18,10 @@ mod tests {
         let b = CircularBuffer::new(1);
         assert_eq!(true, b.is_empty());
         assert_eq!(false, b.is_full());
+    }
+    #[test]
+    fn given_capacity_one_when_put_then_return_true() {
+        let b = CircularBuffer::new(1);
+        assert_eq!(true, b.put(42));
     }
 }
