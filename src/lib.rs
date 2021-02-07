e75a8f3c6c3e465dea31552c83fd0c0d353873c4
philip.a.markgraf <philip_markgraf@mac.com>
2021-02-07 11:29:58 -0800

    f is_empty

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,19 @@
+struct CircularBuffer {}
+
+impl CircularBuffer {
+    pub fn new(_: usize) -> CircularBuffer {
+        CircularBuffer {}
+    }
+    pub fn is_empty(&self) -> bool { true }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn given_capacity_one_when_created_then_is_empty_true() {
+        let b = CircularBuffer::new(1);
+        assert_eq!(true, b.is_empty());
     }
 }
